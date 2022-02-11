b19ba08a48c4c4e6235c256f69c50e1bf7b98dcb
Huygens <jeferson_oliveira97@hotmail.com>
2022-02-11 03:02:45 -0300

    .

--- RenderingEngine.cpp
+++ RenderingEngine.cpp
@@ -307,21 +307,20 @@ namespace DeferredRenderingEngine
 
     bool RenderingEngine::CreateVertexDeclaration(D3DVERTEXELEMENT9* elements, IDirect3DVertexDeclaration9** vertexdeclaration)
     {
-
         uint32_t numelements = 0;
         while (elements[numelements].Type != D3DDECLTYPE_UNUSED)
         {
             numelements++;
         }
         numelements++;
-        
+
         uint32_t sizeElements = numelements * sizeof(D3DVERTEXELEMENT9);
         uint32_t n = 0;
         for (size_t i = 0; i < m_vertexDeclarations.size(); i++)
         {
             if (m_vertexDeclarations[i].numElements == numelements)
             {
-                if (std::memcmp(m_vertexDeclarations[i].elements, elements, sizeElements)==0)
+                if (std::memcmp(m_vertexDeclarations[i].elements, elements, sizeElements) == 0)
                 {
                     n = i;
                     break;
@@ -349,16 +348,16 @@ namespace DeferredRenderingEngine
                 return false;
             }
             Log::Debug("RenderingEngine::CreateVertexDeclaration - created %i", vertexdeclaration);
-           
+
         }
         else
         {
-          
+
             IDirect3DVertexDeclaration9_AddRef(m_vertexDeclarations[n].vertexdeclaration);
             *vertexdeclaration = m_vertexDeclarations[n].vertexdeclaration;
 
             Log::Debug("RenderingEngine::CreateVertexDeclaration - sharing %i", vertexdeclaration);
-           
+
         }
 
         return true;
@@ -371,11 +370,270 @@ namespace DeferredRenderingEngine
             if (m_vertexDeclarations[n].vertexdeclaration == vertexdeclaration)
             {
                 Log::Debug("RenderingEngine::DeleteVertexDeclaration - %i", vertexdeclaration);
-                IDirect3DVertexDeclaration9_Release(m_vertexDeclarations[n].vertexdeclaration);
+                SAFE_RELEASE(m_vertexDeclarations[n].vertexdeclaration);
                 SAFE_DELETE(m_vertexDeclarations[n].elements);
                 m_vertexDeclarations.erase(m_vertexDeclarations.begin() + n);
                 break;
             }
         }
+    }
+
+    bool RenderingEngine::SetRenderTarget(uint32_t index, IDirect3DSurface9* rendertarget)
+    {
+        if (mCurrentRenderSurface[index] != rendertarget)
+        {
+            mCurrentRenderSurface[index] = rendertarget;
+
+            auto hr = IDirect3DDevice9_SetRenderTarget(RwD3DDevice, index, rendertarget);
+
+            return SUCCEEDED(hr);
+        }
+        return true;
+    }
+
+    bool RenderingEngine::SetDepthStencilSurface(IDirect3DSurface9* depthStencilSurface)
+    {
+        if (mCurrentDepthStencilSurface != depthStencilSurface)
+        {
+            mCurrentDepthStencilSurface = depthStencilSurface;
+            auto hr = RwD3DDevice->SetDepthStencilSurface(depthStencilSurface);
+
+            return SUCCEEDED(hr);
+        }
+
+        return true;
+    }
+
+    bool RenderingEngine::CreateVertexShader(DWORD* function, IDirect3DVertexShader9** shader)
+    {
+        auto hr = RwD3DDevice->CreateVertexShader(function, shader);
+
+        if (SUCCEEDED(hr))
+        {
+            mLastVertexShaderUsed = (void*)0xffffffff;
+        }
+
+        return SUCCEEDED(hr);
+    }
+
+    void RenderingEngine::DeleteVertexShader(IDirect3DVertexShader9* shader)
+    {
+        SAFE_RELEASE(shader);
+    }
+
+    bool RenderingEngine::CreatePixelShader(DWORD* function, IDirect3DPixelShader9** shader)
+    {
+        auto hr = RwD3DDevice->CreatePixelShader(function, shader);
+
+        if (SUCCEEDED(hr))
+        {
+            mLastPixelShaderUsed = (void*)0xffffffff;
+        }
+
+        return SUCCEEDED(hr);
+    }
+
+    void RenderingEngine::DeletePixelShader(IDirect3DPixelShader9* shader)
+    {
+        SAFE_RELEASE(shader);
+    }
+
+    void RenderingEngine::SetStreamSource(uint32_t streamNumber, IDirect3DVertexBuffer9* streamData, uint32_t offset, uint32_t stride)
+    {
+        if (mLastVertexStreamUsed[streamNumber].vertexBuffer != streamData ||
+            mLastVertexStreamUsed[streamNumber].offset != offset ||
+            mLastVertexStreamUsed[streamNumber].stride != stride)
+        {
+            mLastVertexStreamUsed[streamNumber].vertexBuffer = streamData;
+            mLastVertexStreamUsed[streamNumber].offset = offset;
+            mLastVertexStreamUsed[streamNumber].stride = stride;
+
+            RwD3DDevice->SetStreamSource(streamNumber, streamData, offset, stride);
+        }
+    }
+
+    void DeferredRenderingEngine::RenderingEngine::SetStreams(RxD3D9VertexStream* streams, bool useOffsets)
+    {
+        if (useOffsets == false)
+        {
+            for (uint32_t n = 0; n < RWD3D9_MAX_VERTEX_STREAMS; n++)
+            {
+                auto vertexBuffer = reinterpret_cast<IDirect3DVertexBuffer9*>(streams[n].vertexBuffer);
+
+                if (vertexBuffer != nullptr)
+                {
+                    if (mLastVertexStreamUsed[n].vertexBuffer != vertexBuffer ||
+                        mLastVertexStreamUsed[n].offset != 0 ||
+                        mLastVertexStreamUsed[n].stride != streams[n].stride)
+                    {
+                        mLastVertexStreamUsed[n].vertexBuffer = vertexBuffer;
+                        mLastVertexStreamUsed[n].offset = 0;
+                        mLastVertexStreamUsed[n].stride = streams[n].stride;
+
+                        RwD3DDevice->SetStreamSource(n, vertexBuffer, 0, streams[n].stride);
+                    }
+                }
+                else
+                {
+                    if (mLastVertexStreamUsed[n].vertexBuffer != nullptr)
+                    {
+                        mLastVertexStreamUsed[n].vertexBuffer = nullptr;
+                        mLastVertexStreamUsed[n].offset = 0;
+                        mLastVertexStreamUsed[n].stride = 0;
+
+                        RwD3DDevice->SetStreamSource(n, nullptr, 0, 0);
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (uint32_t n = 0; n < RWD3D9_MAX_VERTEX_STREAMS; n++)
+            {
+                auto vertexBuffer = reinterpret_cast<IDirect3DVertexBuffer9*>(streams[n].vertexBuffer);
+
+                if (vertexBuffer != nullptr)
+                {
+                    if (mLastVertexStreamUsed[n].vertexBuffer != vertexBuffer ||
+                        mLastVertexStreamUsed[n].offset != streams[n].offset ||
+                        mLastVertexStreamUsed[n].stride != streams[n].stride)
+                    {
+                        mLastVertexStreamUsed[n].vertexBuffer = vertexBuffer;
+                        mLastVertexStreamUsed[n].offset = streams[n].offset;
+                        mLastVertexStreamUsed[n].stride = streams[n].stride;
+
+                        RwD3DDevice->SetStreamSource(n, vertexBuffer, streams[n].offset, streams[n].stride);
+                    }
+                    else
+                    {
+                        if (mLastVertexStreamUsed[n].vertexBuffer != nullptr)
+                        {
+                            mLastVertexStreamUsed[n].vertexBuffer = nullptr;
+                            mLastVertexStreamUsed[n].offset = 0;
+                            mLastVertexStreamUsed[n].stride = 0;
+
+                            RwD3DDevice->SetStreamSource(n, nullptr, 0, 0);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    void RenderingEngine::SetIndices(IDirect3DIndexBuffer9* indexBuffer)
+    {
+        if (mLastIndexBufferUsed != indexBuffer)
+        {
+            mLastIndexBufferUsed = indexBuffer;
+
+            RwD3DDevice->SetIndices(indexBuffer);
+        }
+    }
+
+    void RenderingEngine::RenderStateFlushCache()
+    {
+        if (mNumRenderStateChanges)
+        {
+            for (size_t i = 0; i < mNumRenderStateChanges; i++)
+            {
+                auto state = mRenderStateChanges[i];
+                auto value = mRenderStateCache[state].value;
+                mRenderStateCache[state].changed = false;
+
+                if (mRenderStateD3D[state] != value)
+                {
+                    mRenderStateD3D[state] = value;
+
+                    RwD3DDevice->SetRenderState(state, value);
+                }
+
+                mNumRenderStateChanges = 0;
+            }
+        }
+
+        if (mNumTextureStageStateChanges)
+        {
+            for (size_t i = 0; i < mNumRenderStateChanges; i++)
+            {
+                auto stage = mTextureStageStateChanges[i].stage;
+                auto type = mTextureStageStateChanges[i].type;
+                auto value = mTextureStageStateCache[stage][type].value;
+                mTextureStageStateCache[stage][type].changed = false;
+
+                if (mTextureStageStateD3D[stage][type] != value)
+                {
+                    mTextureStageStateD3D[stage][type] = value;
+
+                    RwD3DDevice->SetTextureStageState(stage, type, value);
+                }
+            }
+
+            mNumTextureStageStateChanges = 0;
+        }
+    }
+
+    void RenderingEngine::GetRenderState(uint32_t state, void* value)
+    {
+        *reinterpret_cast<uint32_t*>(value) = mRenderStateCache[state].value;
+    }
+
+    void RenderingEngine::SetTextureStageState(uint32_t stage, uint32_t type, uint32_t value)
+    {
+        if (mTextureStageStateCache[stage][type].value != value)
+        {
+            mTextureStageStateCache[stage][type].value = value;
+
+            if (!mTextureStageStateCache[stage][type].changed)
+            {
+                mTextureStageStateCache[stage][type].changed = true;
+                mTextureStageStateChanges[mNumTextureStageStateChanges].stage = stage;
+                mTextureStageStateChanges[mNumTextureStageStateChanges].type = (D3DTEXTURESTAGESTATETYPE)type;
+                mNumTextureStageStateChanges++;
+            }
+        }
+    }
+
+    void RenderingEngine::GetTextureStageState(uint32_t stage, uint32_t type, void* value)
+    {
+        *reinterpret_cast<uint32_t*>(value) = mTextureStageStateCache[stage][type].value;
+    }
+
+    void RenderingEngine::SetSamplerState(uint32_t stage, uint32_t type, uint32_t value)
+    {
+        if (mSamplerStageStateD3D[stage][type] != value)
+        {
+            mSamplerStageStateD3D[stage][type] = value;
+
+            RwD3DDevice->SetSamplerState(stage, (D3DSAMPLERSTATETYPE)type, value);
+        }
+    }
+
+    void RenderingEngine::GetSamplerState(uint32_t stage, uint32_t type, void* value)
+    {
+        *reinterpret_cast<uint32_t*>(value) = mSamplerStageStateD3D[stage][type];
+    }
+
+    void RenderingEngine::ForceSamplerState(uint32_t stage, uint32_t type, uint32_t value)
+    {
+        mSamplerStageStateD3D[stage][type] = value;
+
+        RwD3DDevice->SetSamplerState(stage, (D3DSAMPLERSTATETYPE)type, value);
+    }
+
+    void RenderingEngine::ForceRenderState(uint32_t state, uint32_t value)
+    {
+        mRenderStateCache[state].value = value;
+        mRenderStateD3D[state] = value;
+
+        RwD3DDevice->SetRenderState((D3DRENDERSTATETYPE)state, value);
+    }
+
+    void RenderingEngine::ForceTextureStageState(uint32_t stage, uint32_t type, uint32_t value)
+    {
+        mTextureStageStateCache[stage][type].value = value;
+        mTextureStageStateD3D[stage][type] = value;
+
+        RwD3DDevice->SetTextureStageState(stage, (D3DTEXTURESTAGESTATETYPE)type, value);
     }
 }
--- RenderingEngine.h
+++ RenderingEngine.h
@@ -35,6 +35,41 @@ namespace DeferredRenderingEngine
 		static std::list<IndexBuffer*> mDynamicIndexBufferList;
 
 		static std::vector<rxD3D9VertexDeclarationItem> m_vertexDeclarations;
+
+		static IDirect3DSurface9* mCurrentDepthStencilSurface;
+		static IDirect3DSurface9* mCurrentRenderSurface[8];
+
+		static void* mLastVertexShaderUsed; 
+		static void* mLastPixelShaderUsed;
+
+		static RxD3D9VertexStream mLastVertexStreamUsed[4];
+
+		static IDirect3DIndexBuffer9* mLastIndexBufferUsed;
+
+		typedef struct
+		{
+			uint32_t value;
+			bool changed;
+		} RxStateCache;
+
+		static RxStateCache mRenderStateCache[210];
+		static uint32_t mRenderStateD3D[210];
+		static D3DRENDERSTATETYPE mRenderStateChanges[210];
+		static uint32_t mNumRenderStateChanges;
+
+		static RxStateCache mTextureStageStateCache[RWD3D9_MAX_TEXTURE_STAGES][33];
+		static RwUInt32 mTextureStageStateD3D[RWD3D9_MAX_TEXTURE_STAGES][33];
+
+		typedef struct
+		{
+			uint32_t stage;
+			D3DTEXTURESTAGESTATETYPE type;
+		} RxTextureStageChangeType;
+
+		static uint32_t mNumTextureStageStateChanges;
+		static RxTextureStageChangeType mTextureStageStateChanges[RWD3D9_MAX_TEXTURE_STAGES * 33];
+		static uint32_t mSamplerStageStateD3D[RWD3D9_MAX_TEXTURE_STAGES][14];
+
 	public:
 
 		static bool DynamicVertexBufferListCreate();
@@ -56,10 +91,34 @@ namespace DeferredRenderingEngine
 		static void DynamicVertexBufferRestore();
 		static void DynamicVertexBufferRelease();
 
-
-		static bool CreateVertexDeclaration(D3DVERTEXELEMENT9* elements,
-			IDirect3DVertexDeclaration9** decl);
-
+		static bool CreateVertexDeclaration(D3DVERTEXELEMENT9* elements, IDirect3DVertexDeclaration9** decl);
 		static void DeleteVertexDeclaration(IDirect3DVertexDeclaration9* decl);
+
+		static bool SetRenderTarget(uint32_t index, IDirect3DSurface9* rendertarget);
+		static bool SetDepthStencilSurface(IDirect3DSurface9* depthStencilSurface);
+
+		static bool CreateVertexShader(DWORD* function, IDirect3DVertexShader9** shader);
+		static void DeleteVertexShader(IDirect3DVertexShader9* shader);
+		static bool CreatePixelShader(DWORD* function, IDirect3DPixelShader9** shader);
+		static void DeletePixelShader(IDirect3DPixelShader9* shader);
+
+		static void SetStreamSource(uint32_t streamNumber, IDirect3DVertexBuffer9* streamData, uint32_t offset, uint32_t stride);
+		static void SetStreams(RxD3D9VertexStream* streams, bool useOffsets);
+
+		static void SetIndices(IDirect3DIndexBuffer9* indexBuffer);
+
+		static void RenderStateFlushCache();
+
+		static void GetRenderState(uint32_t state, void* value);
+
+		static void SetTextureStageState(uint32_t stage, uint32_t type, uint32_t value);
+		static void GetTextureStageState(uint32_t stage, uint32_t type, void* value);
+
+		void SetSamplerState(uint32_t stage, uint32_t type, uint32_t value);
+		void GetSamplerState(uint32_t stage, uint32_t type, void* value);
+		void ForceSamplerState(uint32_t stage, uint32_t type, uint32_t value);
+
+		void ForceRenderState(uint32_t state, uint32_t value);
+		void ForceTextureStageState(uint32_t stage, uint32_t type, uint32_t value);
 	};
 }
